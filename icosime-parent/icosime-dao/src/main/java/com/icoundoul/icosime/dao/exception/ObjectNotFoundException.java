b3e6f5f819c016da5eadf7197755dc6bc853de55
icoundoul <icoundoul@ico.com>
2020-07-29 14:05:43 +0200

    move parent pom to the root directory

--- icosime-parent/icosime-dao/src/main/java/com/icoundoul/icosime/dao/exception/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.icoundoul.icosime.dao.exception;
-
-/**
- * This exception is thrown when an object is not found in the persistent layer.
- */
-public final class ObjectNotFoundException extends FinderException {
-}
